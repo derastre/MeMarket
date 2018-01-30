bb7ee401ffb5ae34f98f8ff5f6fce5ea7c7c8f06
aederas <arturoderas05@gmail.com>
2018-01-29 20:55:22 -0600

    MOving "select store" to fragments

--- app/src/main/java/com/me_market/android/memarket/CompaniesListFragment.java
+++ app/src/main/java/com/me_market/android/memarket/CompaniesListFragment.java
@@ -1,31 +1,33 @@
 package com.me_market.android.memarket;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.me_market.android.memarket.models.Company;
+
+import java.util.ArrayList;
+
+public class CompaniesListFragment extends Fragment implements View.OnClickListener{
+
+    public static final String COMPANY_DATA = "com.example.android.memarket.COMPANY_DATA";
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link CompaniesListFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link CompaniesListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class CompaniesListFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private DatabaseReference myRef;
+    private ValueEventListener companiesListener;
+    private ArrayList<Company> companyArrayList;
+    private ListView listView;
 
     private OnFragmentInteractionListener mListener;
 
@@ -33,38 +35,34 @@ public class CompaniesListFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CompaniesListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CompaniesListFragment newInstance(String param1, String param2) {
-        CompaniesListFragment fragment = new CompaniesListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_companies_list, container, false);
+        View myView = inflater.inflate(R.layout.fragment_companies_list, container, false);
+
+        //Floating action button
+        FloatingActionButton companies_fab = myView.findViewById(R.id.add_company_fab);
+        companies_fab.setOnClickListener(this);
+
+        //Setting ListView
+        listView = myView.findViewById(R.id.companyList);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+            }
+        });
+
+        return myView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -91,18 +89,20 @@ public class CompaniesListFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        switch (i) {
+            case R.id.add_company_fab:
+                newCompany();
+                break;
+        }
+
     }
 }
