7840f573ff2ef115879a0e95076b7e3d434cdccf
aederas <arturoderas05@gmail.com>
2018-03-17 13:00:28 -0600

    Shopping list when product name selected goto product details.

--- app/src/main/java/com/me_market/android/memarket/SelectCityActivity.java
+++ app/src/main/java/com/me_market/android/memarket/SelectCityActivity.java
@@ -1,24 +1,43 @@
 package com.me_market.android.memarket;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.me_market.android.memarket.components.BaseActivity;
 
 import org.w3c.dom.Text;
@@ -31,16 +50,26 @@ import static com.me_market.android.memarket.CityListFragment.COUNTRY_CODE;
 import static com.me_market.android.memarket.MainActivity.SHARED_PREF;
 
 public class SelectCityActivity extends BaseActivity implements CountryListFragment.CountryListListener,
-        CityListFragment.CityListListener{
+        CityListFragment.CityListListener, View.OnClickListener {
 
+    private static final int RC_HANDLE_LOCATION_PERM = 3;
+    private static final String TAG = "Location";
+    private static final int REQUEST_CHECK_SETTINGS = 9;
+    private static final String REQUESTING_LOCATION_UPDATES_KEY = "mRequestingLocationUpdates";
     private FusedLocationProviderClient mFusedLocationClient;
-    private TextView cityName;
-    private TextView countryName;
+    private String cityName;
+    private String countryName;
+    private String countryCode;
+    private boolean mRequestingLocationUpdates;
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback;
+    private View dialogView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_city);
+
 
         if (findViewById(R.id.fragment_container_city) != null) {
 
@@ -68,14 +97,21 @@ public class SelectCityActivity extends BaseActivity implements CountryListFragm
             ab.setDisplayHomeAsUpEnabled(true);
         }
 
+        //View where we add a new city
+        dialogView = getLayoutInflater().inflate(R.layout.add_country_city_dialog, null);
+
+        //FAB Button
+        FloatingActionButton addCityFab = (FloatingActionButton) findViewById(R.id.add_city_fab);
+        addCityFab.setOnClickListener(this);
+
     }
 
     @Override
-    public void onCountrySelected(String s){
+    public void onCountrySelected(String s) {
         //Start Stores List Fragment and pass the selected company
         CityListFragment cityListFragment = new CityListFragment();
         Bundle args = new Bundle();
-        args.putString(COUNTRY_CODE,s);
+        args.putString(COUNTRY_CODE, s);
         cityListFragment.setArguments(args);
 
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
@@ -88,8 +124,8 @@ public class SelectCityActivity extends BaseActivity implements CountryListFragm
     }
 
     @Override
-    public void onCitySelected(String city, String country){
-        SharedPreferences sharedPref = getSharedPreferences(SHARED_PREF,Context.MODE_PRIVATE);
+    public void onCitySelected(String city, String country) {
+        SharedPreferences sharedPref = getSharedPreferences(SHARED_PREF, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
         editor.putString(getString(R.string.city_pref), city);
         editor.putString(getString(R.string.country_pref), country);
@@ -98,44 +134,194 @@ public class SelectCityActivity extends BaseActivity implements CountryListFragm
         finish();
     }
 
-    private void locationFromGPS() {
+    private void getlocationFromGPS() {
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        showProgressDialog(getString(R.string.loading), this);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        showProgressDialog(getString(R.string.loading), SelectCityActivity.this);
+        if (ActivityCompat.checkSelfPermission(SelectCityActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mFusedLocationClient.getLastLocation()
                     .addOnSuccessListener(this, new OnSuccessListener<Location>() {
                         @Override
                         public void onSuccess(Location location) {
                             // Got last known location. In some rare situations this can be null.
                             if (location != null) {
-
-                                Geocoder geocoder = new Geocoder(SelectCityActivity.this, Locale.getDefault());
-                                try {
-                                    List<Address> list = geocoder.getFromLocation(
-                                            location.getLatitude(), location.getLongitude(), 1);
-                                    if (list != null && list.size() > 0) {
-                                        Address address = list.get(0);
-                                        String text = "Country Code " + address.getCountryCode() + "\n"
-                                                + "Country Name " + address.getCountryName() + "\n"
-                                                + "Feature Name " + address.getFeatureName() + "\n"
-                                                + "Locality " + address.getLocality() + "\n"
-                                                + "Locale " + address.getLocale() + "\n"
-                                                + "Admin area " + address.getAdminArea() + "\n"
-                                                + "Phone " + address.getPhone() + "\n"
-                                                + "Postal code " + address.getPostalCode() + "\n"
-                                                + "Premises " + address.getPremises() + "\n"
-                                                + "Sub admin Area " + address.getSubAdminArea() + "\n"
-                                                + "Sub locality " + address.getSubLocality() + "\n";
-                                        cityName.setText(text);
-                                        countryName.setText(address.getAddressLine(0));
-                                        hideProgressDialog();
-                                    }
-                                } catch (IOException e) {
-                                    Log.e("Location", "Impossible to connect to Geocoder", e);
-                                }
+                                findCountryCityGeocode(location);
+                            } else {
+                                findCurrentLocation();
                             }
                         }
                     });
+        } else {
+            requestLocationPermission();
+        }
+    }
+
+    private void findCountryCityGeocode(Location location) {
+        Geocoder geocoder = new Geocoder(SelectCityActivity.this, Locale.getDefault());
+        try {
+            List<Address> list = geocoder.getFromLocation(
+                    location.getLatitude(), location.getLongitude(), 1);
+            if (list != null && list.size() > 0) {
+                Address address = list.get(0);
+                countryName = address.getCountryName();
+                countryCode = address.getCountryCode();
+                cityName = address.getSubAdminArea();
+                TextView textView = dialogView.findViewById(R.id.country_name_textview);
+                textView.setText(countryName);
+                textView = dialogView.findViewById(R.id.country_code_textview);
+                textView.setText(countryCode);
+                textView = dialogView.findViewById(R.id.city_name_textview);
+                textView.setText(cityName);
+
+                hideProgressDialog();
+            }
+        } catch (IOException e) {
+            Log.e("Location", "Impossible to connect to Geocoder", e);
+        }
+    }
+
+    private void findCurrentLocation() {
+        createLocationRequest();
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                Location location = locationResult.getLastLocation();
+
+                //for (Location location : locationResult.getLocations()) {
+                findCountryCityGeocode(location);
+                //}
+            }
+
+            ;
+        };
+    }
+
+    private void add_new_city() {
+
+        AlertDialog dialog;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(dialogView);
+        getlocationFromGPS();
+        // Set up the buttons
+        builder.setPositiveButton(R.string.add_button, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+
+            }
+        });
+        builder.setNegativeButton(R.string.cancel_button, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        dialog = builder.create();
+        dialog.show();
+    }
+
+    private void requestLocationPermission() {
+        Log.w(TAG, "Location permission is not granted. Requesting permission");
+
+        final String[] permissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
+
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            ActivityCompat.requestPermissions(SelectCityActivity.this, permissions, RC_HANDLE_LOCATION_PERM);
+            this.recreate();
+            return;
+        }
+
+        final Activity thisActivity = this;
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(thisActivity, permissions, RC_HANDLE_LOCATION_PERM);
+            }
+        };
+
+        findViewById(R.id.camera_view).setOnClickListener(listener);
+        Snackbar.make(findViewById(R.id.fragment_container_city), R.string.permission_location_rationale,
+                Snackbar.LENGTH_INDEFINITE)
+                .setAction(R.string.ok, listener)
+                .show();
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+
+        SettingsClient client = LocationServices.getSettingsClient(SelectCityActivity.this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+        task.addOnSuccessListener(SelectCityActivity.this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                // All location settings are satisfied. The client can initialize
+                // location requests here.
+                // ...
+                if (ActivityCompat.checkSelfPermission(SelectCityActivity.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                            mLocationCallback,
+                            null /* Looper */);
+                }
+            }
+        });
+
+        task.addOnFailureListener(SelectCityActivity.this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    // Location settings are not satisfied, but this can be fixed
+                    // by showing the user a dialog.
+                    try {
+                        // Show the dialog by calling startResolutionForResult(),
+                        // and check the result in onActivityResult().
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(SelectCityActivity.this,
+                                REQUEST_CHECK_SETTINGS);
+                    } catch (IntentSender.SendIntentException sendEx) {
+                        // Ignore the error.
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    private void stopLocationUpdates() {
+        if (mFusedLocationClient != null && mLocationCallback != null) {
+            mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(REQUESTING_LOCATION_UPDATES_KEY,
+                mRequestingLocationUpdates);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.add_city_fab) {
+            add_new_city();
         }
     }
 }
